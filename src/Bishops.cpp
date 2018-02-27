3e7842605c9495ea6509ae42cd04f6abff65cbf1
Ireneo <ireneo@ireneo.ireneo>
2018-02-27 13:50:17 +0100

    pieces declaration

---
+++ src/Bishops.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Piece.h"
+using namespace std;
+
+class Bishops:public Piece {
+
+public:
+
+};
---
+++ src/Rook.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Piece.h"
+using namespace std;
+
+class Rock:public Piece {
+
+public:
+
+};
