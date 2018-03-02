8022016ece59fec3b7694b78bf429c377f2fe89e
DSL <sanchezlealdaniel@gmail.com>
2018-03-02 12:24:45 +0100

    minor fixes

--- src/Board.cpp
+++ src/Board.cpp
@@ -49,11 +49,11 @@ Cell Board::getCell(unsigned short x, unsigned short y) {
 }
 
 bool Board::valid(unsigned short x0, unsigned short y0, unsigned short x, unsigned short y){
-    if (x0 == x && y0 == y
-        || x0 > 7 && y0 > 7
-        || x > 7 && y > 7
-        || !getCell(x0,y0)->valid(x,y)
-        || getCell(x0,y0)->isEmpty()){
+    if ((x0 == x && y0 == y)
+        || (x0 > 7 && y0 > 7)
+        || (x > 7 && y > 7)
+        || (!getCell(x0,y0).getPiece()->valid(x,y))
+        || (getCell(x0,y0).isEmpty())){
             return false;
     }
     return true;
