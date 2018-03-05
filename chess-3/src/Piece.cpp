0ad22681170b380676c63ae2b72d62edc58355f4
Ireneo <ireneo@ireneo.ireneo>
2018-03-05 09:17:30 +0100

    update

---
+++ chess-3/src/Piece.cpp
@@ -0,0 +1,36 @@
+#include "Piece.h"
+
+
+
+Piece::Piece() {}
+
+std::string Piece::getColour(){
+  if(black) {
+      return "Black";
+  }else {
+      return "White";
+  }
+}
+
+std::string Piece::getName(){
+  switch(figure){
+    case 'P':
+      return "pawn";
+      break;
+    case 'K':
+      return "king";
+      break;
+    case 'Q':
+      return "queen";
+      break;
+    case 'B':
+      return "bishop";
+      break;
+    case 'R':
+      return "rook";
+      break;
+    case 'k':
+      return "knight";
+      break;
+  }
+}
