8820a4e4d220e4ba3841fd1c6a305933042ec6fc
Ireneo <ireneo@ireneo.ireneo>
2018-03-05 14:00:10 +0100

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,19 @@ unsigned short charToShort(char letter){
 
 int main() {
     Board::getInstance().init();
+
+    do{
+        std::string piecexy;
+        std::string coord;
+        std::cout << "Ficha que quieres mover" << '\n';
+        cin>>piecexy;
+        std::cout << "Donde quieres moverla" << '\n';
+        cin>>coord;
+        std::cout << charToShort(piecexy[0]) << '\n';
+        Board::getInstance().moveOnBoard(charToShort(piecexy[0]),charToShort(piecexy[1]),
+        charToShort(coord[0]),charToShort(coord[1])); // Pawn1
+    }while(true);
+
     // COLLISION TEST FOR ROOK
     // Board::getInstance().moveOnBoard(1,1, 2,1); // Pawn1
     // Board::getInstance().moveOnBoard(0,2, 1,1); // Pawn1
@@ -41,27 +54,4 @@ int main() {
 //     Board::getInstance().moveOnBoard(0,2, 1,1); // Bishop1
 //     Board::getInstance().moveOnBoard(0,1, 2,0); // Knight1
 //     Board::getInstance().moveOnBoard(2,1, 0,1); // Rook1 FAIL
-    do{
-        std::string piecexy;
-        std::string coord;
-        std::cout << "Ficha que quieres mover" << '\n';
-        cin>>piecexy;
-        std::cout << "Donde quieres moverla" << '\n';
-        cin>>coord;
-        std::cout << charToShort(piecexy[0]) << '\n';
-        Board::getInstance().moveOnBoard(charToShort(piecexy[0]),charToShort(piecexy[1]),
-        charToShort(coord[0]),charToShort(coord[1])); // Pawn1
-
-
-    }while(true);
-
-    /*do {
-        std::cout << "Ficha que quieres mover" << '\n';
-        std::string piecexy;
-        cin<<piecexy;
-        std::cout << "Donde quieres moverla" << '\n';
-        std::string coord;
-        Board::getInstance().moveOnBoard((unsigned short)atoi(piecexy[0]),(unsigned short)atoi(piecexy[1]),(unsigned short) atoi(coord[0]),(unsigned short)atoi(coord[1])); // Pawn1
-    } while (true);*/
-
 }
