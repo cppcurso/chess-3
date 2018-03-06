ecf9e56510cfe3da51c0e350a2811dca9345f568
JorgeC <jorge@jorge.jorge>
2018-03-06 11:54:03 +0100

    minor fixes

--- src/Piece.h
+++ src/Piece.h
@@ -12,7 +12,6 @@ protected:
     bool black;
     char figure;
 public:
-    //Piece();
     Piece(unsigned short x, unsigned short y , bool colour = true);
     std::string getColour();
     std::string getName();
@@ -20,12 +19,4 @@ public:
     virtual void move (unsigned short x, unsigned short y);
     virtual bool valid (unsigned short x, unsigned short y) = 0;
 };
-/* TO DO
-board.cells[1][5].getPiece()->move(6,5);
-board.cells[6][5].setPointerToPiece(*(board.cells[1][5].getPiece()));
-board.cells[1][5].setCellToNull();
-board.print();
-//std::cout << board.cells[6][5].getPiece()->x;
-//std::cout << board.cells[6][5].getPiece()->y;*/
-
 #endif
