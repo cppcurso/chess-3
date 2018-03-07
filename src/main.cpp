3566083bb49ed28e40ab9e223d09db78564cd945
Nacho <ignaciomorenoadt@gmail.com>
2018-03-07 09:06:37 +0100

    main modified for Gtest

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,17 @@
 #include "Rook.h"
 #include "Game.h"
 #include "Chess.h"
+#include "external/gtest/gtest.h"
 
 
-int main() {
-    Board::getInstance().init();
-
-    Chess chessGame;
-    std::cout << "Partida 1" << '\n';
-        chessGame.play();
-
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
+
+// int main() {
+//     Board::getInstance().init();
+//     Chess chessGame;
+//     std::cout << "Partida 1" << '\n';
+//     chessGame.play();
+// }
